ce396696c5d3f7c8c5eada42aee957adcf135dac
Tamilarasan <kingtamilarasan24@gmail.com>
2025-04-24 11:42:15 +0530

    Update environment.common.ts

--- src/environments/environment.common.ts
+++ src/environments/environment.common.ts
@@ -1,13 +1,6 @@
 export const commonEnvironment = {
   version: '0.0.1',
   firebase: {
-     
-    apiKey: "AIzaSyATjdhinjQiIGTZnQ5rzUk1U4VBw1d5f78",
-    authDomain: "srf-career.firebaseapp.com",
-    projectId: "srf-career",
-    storageBucket: "srf-career.firebasestorage.app",
-    messagingSenderId: "368524985444",
-    appId: "1:368524985444:web:a22895270cb36c3e900b71",
-    measurementId: "G-YYS4GPWGNB"
+ 
   },
 };
